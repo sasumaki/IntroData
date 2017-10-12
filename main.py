911b45519fd6719f926002af838e8eec0e4f56dd
Sasu Mäkinen <sasumaki@dhcp-asennus-kumpula-28.helsinki.fi>
2017-10-12 21:28:51 +0300

    semi ok kartta

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 import matplotlib.pyplot as plt
 import geopandas as gpd
 import geopandas_osm.osm as osm
-
+from shapely.geometry.polygon import LinearRing, Polygon
 
 
 
@@ -26,17 +26,26 @@ def plotMetropolitan():
 	df_road = gpd.read_file("metropolitan/metropolitan_finland_roads_gen1.geojson")
 
 	df_road = df_road.to_crs(epsg=3067)
+	df_admin = gpd.read_file("metropolitan/metropolitan_finland_admin.geojson")
+	df_admin = df_admin.to_crs(epsg=3067)
+
+
+	ax = df_road.plot(color="blue", alpha=1,linewidth=0.5, zorder=-1)
+
+	for i in xrange(0,len(df_admin)):
+		boundary = df_admin.ix[i].geometry
+		x,y = boundary.exterior.xy
+		ax.plot(x,y,color="black",linewidth=0.8,zorder=0)
+
 	
 
-	ax = df_road.plot(color="black", alpha=1)
-
 	#This is for creating a colormap of the populations
-	cm = plt.cm.get_cmap('Wistia')
+	cm = plt.cm.get_cmap('YlOrRd')
 	vmin = df["vaesto"].argmin()
 	vmax = (df["vaesto"].argmax()-df["vaesto"].mean())/2
 
-	plt.scatter(df.xkoord, df.ykoord, c=df["vaesto"], s=25, cmap = cm,vmin = vmin, vmax = vmax)	
-	ax.set_axis_bgcolor("gray")
+	plt.scatter(df.xkoord, df.ykoord, c=df["vaesto"], s=20, alpha=1, cmap = cm,vmin = vmin, vmax = vmax, zorder=1)	
+	ax.set_axis_bgcolor("lightgray")
 	plt.show()
 
 
