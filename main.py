bfb6a01b1a04ac0e714cae7f781c6b15e0644725
Sasu Mäkinen <sasumaki@dhcp-asennus-kumpula-28.helsinki.fi>
2017-10-12 20:28:06 +0300

    osm sekoilu etenee

--- main.py
+++ main.py
@@ -23,14 +23,11 @@ def plotMetropolitan():
 	#codes: Helsinki = 91, Espoo = 49, Vantaa = 92, Kauniainen = 235
 	df = df[(df["kunta"] == 91) | (df["kunta"] == 49) | (df["kunta"] == 92) | (df["kunta"] == 235)]
 
-	#df_building = gpd.read_file("metropolitan/metropolitan_finland_buildings.geojson")
 	df_road = gpd.read_file("metropolitan/metropolitan_finland_roads_gen1.geojson")
 
-	#df_building = df_building.to_crs(epsg=3067)
 	df_road = df_road.to_crs(epsg=3067)
 	
 
-	#df_building.plot(color="blue", alpha=.7)
 	ax = df_road.plot(color="black", alpha=1)
 
 	#This is for creating a colormap of the populations
