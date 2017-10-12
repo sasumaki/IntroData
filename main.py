dbcb7708f8a718c9b81eaf4600a9e290ea9213b6
Sasu Mäkinen <sasumaki@dhcp-asennus-kumpula-28.helsinki.fi>
2017-10-12 19:03:18 +0300

    OpenStreetMap sekoilua

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 # Python modules
 import matplotlib.pyplot as plt
 import geopandas as gpd
+import geopandas_osm as osm
+
+
+
 
 # Own modules
 import readFiles
@@ -12,16 +16,24 @@ def main():
 
 
 #Plot the metropolitan area colormapped by population and background map with postal-areas
+#EPSG:3067: ETRS89 / ETRS-TM35FIN
+
 def plotMetropolitan():
 	df=readFiles.readFiles()
 	#codes: Helsinki = 91, Espoo = 49, Vantaa = 92, Kauniainen = 235
 	df = df[(df["kunta"] == 91) | (df["kunta"] == 49) | (df["kunta"] == 92) | (df["kunta"] == 235)]
 
-	metropolitan = gpd.read_file('Postinumeroalueet_2015_shp/Postinumeroalueet_2015_shp.shp')
-	metropolitan = metropolitan.to_crs(epsg=3067) #EPSG:3067:  ETRS89 / ETRS-TM35FIN / EUREF-FIN
+	df_map = gpd.read_file("helsinki_finland/helsinki_finland_roads.geojson")
 
-	metropolitan.plot(color = "grey")
+	admin_df = gpd.read_file('helsinki_finland/helsinki_finland_admin.geojson')
+	print(admin_df[admin_df["admin_leve" == 8.0]])
+	sg_boundary = admin_df[admin_df["admin_leve" == 8.0]].geometry
 	
+	df_map = geopandas_osm.osm.query_osm('way', sg_boundary, recurse='down', tags='highway')
+
+	df_map = df_map.to_crs(epsg=3067)
+	df_map.plot()
+
 	#This is for creating a colormap of the populations
 	cm = plt.cm.get_cmap('Wistia')
 	vmin = df["vaesto"].argmin()
