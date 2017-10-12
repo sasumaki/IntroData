4c7cbcccedf65a830b86169b845dcb87c67d1ab0
Sasu Mäkinen <sasumaki@dhcp-asennus-kumpula-28.helsinki.fi>
2017-10-12 20:24:58 +0300

    osm sekoilu etenee

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # Python modules
 import matplotlib.pyplot as plt
 import geopandas as gpd
-import geopandas_osm as osm
+import geopandas_osm.osm as osm
 
 
 
@@ -16,30 +16,30 @@ def main():
 
 
 #Plot the metropolitan area colormapped by population and background map with postal-areas
-#EPSG:3067: ETRS89 / ETRS-TM35FIN
+#EPSG:3067: ETRS89 / ETRS-TM35FIN / EUREF-FIN
 
 def plotMetropolitan():
 	df=readFiles.readFiles()
 	#codes: Helsinki = 91, Espoo = 49, Vantaa = 92, Kauniainen = 235
 	df = df[(df["kunta"] == 91) | (df["kunta"] == 49) | (df["kunta"] == 92) | (df["kunta"] == 235)]
 
-	df_map = gpd.read_file("helsinki_finland/helsinki_finland_roads.geojson")
+	#df_building = gpd.read_file("metropolitan/metropolitan_finland_buildings.geojson")
+	df_road = gpd.read_file("metropolitan/metropolitan_finland_roads_gen1.geojson")
 
-	admin_df = gpd.read_file('helsinki_finland/helsinki_finland_admin.geojson')
-	print(admin_df[admin_df["admin_leve" == 8.0]])
-	sg_boundary = admin_df[admin_df["admin_leve" == 8.0]].geometry
+	#df_building = df_building.to_crs(epsg=3067)
+	df_road = df_road.to_crs(epsg=3067)
 	
-	df_map = geopandas_osm.osm.query_osm('way', sg_boundary, recurse='down', tags='highway')
 
-	df_map = df_map.to_crs(epsg=3067)
-	df_map.plot()
+	#df_building.plot(color="blue", alpha=.7)
+	ax = df_road.plot(color="black", alpha=1)
 
 	#This is for creating a colormap of the populations
 	cm = plt.cm.get_cmap('Wistia')
 	vmin = df["vaesto"].argmin()
-	vmax = df["vaesto"].argmax()
+	vmax = (df["vaesto"].argmax()-df["vaesto"].mean())/2
 
-	plt.scatter(df.xkoord, df.ykoord, c=df["vaesto"], s=8, cmap = cm,vmin = vmin, vmax = vmax, alpha=.8)	
+	plt.scatter(df.xkoord, df.ykoord, c=df["vaesto"], s=25, cmap = cm,vmin = vmin, vmax = vmax)	
+	ax.set_axis_bgcolor("gray")
 	plt.show()
 
 
